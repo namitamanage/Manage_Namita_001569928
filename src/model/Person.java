df04bf82ee800a805e4f1b97ba6ebbdd4ffccdb2
namitamanage <91434999+namitamanage@users.noreply.github.com>
2021-09-26 20:00:35 -0400

    add java files

---
+++ src/model/Person.java
@@ -0,0 +1,173 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.io.File;
+import java.util.Date;
+
+/**
+ *
+ * @author namita
+ */
+public class Person {
+    private String Name;
+    private String Address;
+    private Date DoB;
+    private Long PhNo;
+    private String FaxNo;
+    private String Email;
+    private String ssn;
+    private String MedicalNo;
+    private String HealthPlanNo;
+    private String BankAccNo;
+    private String CertificateNo;
+    private String VehicleIdentifier;
+    private String Deviceidentifier;
+    private String LinkedIn;
+    private String ipaddress;
+    private String uidcode;
+    private File photo;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String Address) {
+        this.Address = Address;
+    }
+
+    public Date getDoB() {
+        return DoB;
+    }
+
+    public void setDoB(Date DoB) {
+        this.DoB = DoB;
+    }
+
+  
+
+    public Long getPhNo() {
+        return PhNo;
+    }
+
+    public void setPhNo(Long PhNo) {
+        this.PhNo = PhNo;
+    }
+
+    public String getFaxNo() {
+        return FaxNo;
+    }
+
+    public void setFaxNo(String FaxNo) {
+        this.FaxNo = FaxNo;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getssn() {
+        return ssn;
+    }
+
+    public void setssn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getMedicalNo() {
+        return MedicalNo;
+    }
+
+    public void setMedicalNo(String MedicalNo) {
+        this.MedicalNo = MedicalNo;
+    }
+
+    public String getHealthPlanNo() {
+        return HealthPlanNo;
+    }
+
+    public void setHealthPlanNo(String HealthPlanNo) {
+        this.HealthPlanNo = HealthPlanNo;
+    }
+
+    public String getBankAccNo() {
+        return BankAccNo;
+    }
+
+    public void setBankAccNo(String BankAccNo) {
+        this.BankAccNo = BankAccNo;
+    }
+
+    public String getCertificateNo() {
+        return CertificateNo;
+    }
+
+    public void setCertificateNo(String CertificateNo) {
+        this.CertificateNo = CertificateNo;
+    }
+
+    public String getVehicleIdentifier() {
+        return VehicleIdentifier;
+    }
+
+    public void setVehicleIdentifier(String VehicleIdentifier) {
+        this.VehicleIdentifier = VehicleIdentifier;
+    }
+
+    public String getDeviceidentifier() {
+        return Deviceidentifier;
+    }
+
+    public void setDeviceidentifier(String Deviceidentifier) {
+        this.Deviceidentifier = Deviceidentifier;
+    }
+
+    public String getLinkedIn() {
+        return LinkedIn;
+    }
+
+    public void setLinkedIn(String LinkedIn) {
+        this.LinkedIn = LinkedIn;
+    }
+
+    public String getipaddress() {
+        return ipaddress;
+    }
+
+    public void setipaddress(String ipaddress) {
+        this.ipaddress = ipaddress;
+    }
+
+    public String getuidcode() {
+        return uidcode;
+    }
+
+    public void setuidcode(String uidcode) {
+        this.uidcode = uidcode;
+    }
+
+    public File getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(File photo) {
+        this.photo = photo;
+    }
+
+    
+   
+}
